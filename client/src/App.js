abf35c3fc9b9d1bdac88c452128770d7e5907bdc
dave8git <dave8git@gmail.com>
2023-01-20 15:47:00 +0100

    initial commit - after previous version client didn't upload

---
+++ client/src/App.js
@@ -0,0 +1,70 @@
+import io from 'socket.io-client';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const App = () => {
+  const [socket, setSocket] = useState(null);
+  const [taskName, setTaskName] = useState('');
+  const emit = true;
+
+  useEffect(() => {
+    const socket = io('localhost:8000');
+    setSocket(socket);
+    socket.on('addTask', (task) => addTask(task));
+    socket.on('removeTask', (id) => removeTask(id));
+    socket.on('updateData', (tasks) => updateTasks(tasks));
+  }, []);
+  
+  const [ tasks, setTasks ] = useState([]);
+
+  const removeTask = (id, emit) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
+    if (emit) {
+      socket.emit('removeTask', id)
+    }
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    addTask({id: uuidv4(), name: taskName});
+    socket.emit('addTask', {id: uuidv4, name: taskName });
+  }
+  
+  const addTask = task => {
+    setTasks((tasks) => [...tasks, task]);
+  };
+
+  const updateTasks = task => {
+    setTasks(tasks);
+  }
+
+  return (
+    <div className="App">
+  
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+  
+      <section className="tasks-section" id="tasks-section">
+        <h2>Tasks</h2>
+  
+        <ul className="tasks-section__list" id="tasks-list">
+          {tasks.map((task) => 
+              <li className="task" key={task.id}>{task.name}<button className="btn btn--red" onClick={() => removeTask(task.id, emit)}>Remove</button></li>
+          )}
+          
+          {/* <li className="task">Go out with a dog <button className="btn btn--red">Remove</button></li> */}
+        </ul>
+  
+        <form id="add-task-form" onSubmit={submitForm}>
+          <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+          <button className="btn" type="submit">Add</button>
+        </form>
+  
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express(); 
+const socket = require('socket.io');
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+});
+
+const tasks = [];
+const io = socket(server);
+
+io.on('connection', (socket) => { //connection - jest to event wysyłany automatycznie przez każdy socket w momencie inicjacji połączenia. 
+    socket.emit('updateData', tasks); // user uruchamia aplikację i od razu otrzymuje wszystkie taski w liście (przechowywane w tablicy tasks)
+
+    socket.on('addTask', (task) => {
+
+        socket.broadcast.emit('addTask', task); // emituję zdarzenie addTask do wszystkich socketów *Z WYJĄTKIEM* tego, który dodał task
+    });
+
+    socket.on('removeTask', (id) => {
+        const taskId = tasks.find(task => task.id === id); // wyszukujemy task w tablicy tasks o zgodnym id
+        tasks.splice(taskId, 1); // usuwamy task o zgodnym id
+        socket.broadcast.emit('removeTask', id); // emituję zdarzenie removeTask do wszystkich socketów *Z WYJĄTKIEM* tego, który usunął task
+    })
+
+})
+
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...'});
+});
